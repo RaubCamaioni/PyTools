dd6d941cec0309876aa1ff40f5647484526286df
Raub Camaioni <raubcameo@gmail.com>
2024-11-17 16:46:54 -0500

    video audio converter

--- app/tools/video_to_audio.py
+++ app/tools/video_to_audio.py
@@ -6,7 +6,7 @@ import av
 def video_to_audio(video_file: Path) -> Path:
     with closing(av.open(video_file)) as src_c:
         for i, stream in enumerate(src_c.streams.audio):
-            output_path = video_file.with_name(video_file.stem + f"_{i}.mp4")
+            output_path = video_file.with_name(video_file.stem + f"_{i}.mp3")
             with closing(av.open(output_path, mode="w")) as dst_c:
                 dst_s = dst_c.add_stream("mp3", rate=stream.rate)
                 for packet in src_c.demux(stream):
