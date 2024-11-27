5895b7bf22550b887c87f04c052b5f08257399a0
Raub Camaioni <raubcameo@gmail.com>
2024-11-27 22:04:10 +0000

    moving to database

--- app/app/routes/tools.py
+++ app/app/routes/tools.py
@@ -15,7 +15,6 @@ import secrets
 import logging
 import shutil
 import string
-import json
 import time
 import stat
 import ast
@@ -58,79 +57,84 @@ class FunctionVisitor(ast.NodeVisitor):
 
 @dataclass
 class Tool:
-    file: str
     name: str
     form: str
     source: str
     arg_types: Dict[str, Type]
 
 
+def load_tool(tool_name: str, tool_source: str):
+    tree = ast.parse(tool_source)
+
+    entry_node = None
+    for node in FunctionVisitor(tree):
+        if tool_name == node.name:
+            entry_node = node
+
+    if entry_node is None:
+        logger.warning(f"no entrypoint found in: {tool_name}")
+        return None
+
+    arg_form = []
+    arg_types = {}
+
+    default_args = [None] * len(entry_node.args.args)
+    for i, default in enumerate(entry_node.args.defaults[::-1]):
+        default_args[-i - 1] = ast.unparse(default)
+
+    skip_tool = False
+    for arg, default in zip(entry_node.args.args, default_args):
+        if not arg.annotation:
+            logger.warning(f"{tool_name} arguments must be annotated")
+            skip_tool = True
+            break
+
+        arg_name, arg_type = arg.arg, ast.unparse(arg.annotation)
+
+        base_type = arg_type not in ["int", "float", "str", "Path"]
+        literal_type = "Literal" not in arg_type
+        if base_type and literal_type:
+            logger.warning(f"invalid arg_type {arg_type} in {tool_name}")
+            skip_tool = True
+            break
+
+        if not base_type:
+            arg_types[arg_name] = eval(arg_type)
+        elif not literal_type:
+            arg_types[arg_name] = eval(render.parser_literal(arg_type))
+
+        arg_form.append(render.form_group(arg.arg, arg_type, default))
+
+    if skip_tool:
+        return None
+
+    tool_arg_string = []
+    for k, v in arg_types.items():
+        tool_arg_string.append(f"  {k}: {v}")
+    tool_arg_string = "\n".join(tool_arg_string)
+    logger.info(f"Loading Tool: {tool_name}\n{tool_arg_string}")
+
+    return Tool(
+        name=tool_name,
+        source=tool_source,
+        arg_types=arg_types,
+        form="\n".join(arg_form),
+    )
+
+
 def load_tools(tools_directory: Path) -> dict[str, Tool]:
-    
     tools: dict[str, Tool] = {}
 
     for tool_file in tools_directory.glob("*.py"):
         tool_name = tool_file.stem
 
         with open(tool_file, "r") as f:
-            source = "".join(f.readlines()).strip()
+            tool_source = "".join(f.readlines()).strip()
 
-        tree = ast.parse(source)
-        entry_node = None
-        for node in FunctionVisitor(tree):
-            if tool_name == node.name:
-                entry_node = node
+        tool = load_tool(tool_name, tool_source)
 
-        if entry_node is None:
-            logger.warning(f"no entrypoint found in: {tool_name}")
+        if tool is None:
             continue
-
-        arg_form = []
-        arg_types = {}
-
-        default_args = [None] * len(entry_node.args.args)
-        for i, default in enumerate(entry_node.args.defaults[::-1]):
-            default_args[-i - 1] = ast.unparse(default)
-
-        skip_tool = False
-        for arg, default in zip(entry_node.args.args, default_args):
-            if not arg.annotation:
-                logger.warning(f"{tool_name} arguments must be annotated")
-                skip_tool = True
-                break
-
-            arg_name, arg_type = arg.arg, ast.unparse(arg.annotation)
-
-            base_type = arg_type not in ["int", "float", "str", "Path"]
-            literal_type = "Literal" not in arg_type
-            if base_type and literal_type:
-                logger.warning(f"invalid arg_type {arg_type} in {tool_name}")
-                skip_tool = True
-                break
-
-            if not base_type:
-                arg_types[arg_name] = eval(arg_type)
-            elif not literal_type:
-                arg_types[arg_name] = eval(render.parser_literal(arg_type))
-
-            arg_form.append(render.form_group(arg.arg, arg_type, default))
-
-        if skip_tool:
-            continue
-
-        tool_arg_string = []
-        for k, v in arg_types.items():
-            tool_arg_string.append(f"  {k}: {v}")
-        tool_arg_string = "\n".join(tool_arg_string)
-        logger.info(f"Loading Tool: {tool_name}\n{tool_arg_string}")
-
-        tool = Tool(
-            file=tool_file,
-            name=tool_name,
-            source=source,
-            arg_types=arg_types,
-            form="\n".join(arg_form),
-        )
 
         tools[tool_name] = tool
 
@@ -163,8 +167,6 @@ async def get_temp_dir():
 
 @router.route("/", methods=["GET", "POST"])
 async def read_root(request: Request):
-    user = request.session.get("user")
-
     return TEMPLATES.TemplateResponse(
         "index.html",
         {
@@ -177,14 +179,14 @@ async def read_root(request: Request):
 
 @router.get("/tools", response_class=JSONResponse)
 async def get_tools(request: Request):
-    tools = request.app.state.tools
+    tools: dict[str, Tool] = request.app.state.tools
     content = render.list_item(request.scope.get("root_path"), tools)
     return HTMLResponse(content=content)
 
 
 @router.get("/tool/{tool_name}", response_class=HTMLResponse)
 async def entrypoint_page(request: Request, tool_name: str):
-    tools = request.app.state.tools
+    tools: dict[str, Tool] = request.app.state.tools
     if not (tool := tools.get(tool_name, None)):
         raise HTTPException(status_code=404, detail="Tool Not Found")
 
@@ -207,12 +209,8 @@ async def run_isolated(
     request: Request,
     tool_name: str,
     temp_dir: Annotated[Path, Depends(get_temp_dir)],
-    session: SessionDep,
 ) -> str:
-    
-    user = User(**(request.session.get("user") or {}))
-
-    tools = request.app.state.tools
+    tools: dict[str, Tool] = request.app.state.tools
     if tool_name not in tools:
         raise HTTPException(status_code=404, detail="Tool Not Found")
 
@@ -224,13 +222,18 @@ async def run_isolated(
         form_data = {}
 
     kwargs = {}
+    temp_tool = (temp_dir / tool.name).with_suffix(".py")
+    with open(temp_tool, "w") as f:
+        f.write(tool.source)
+
     for key, value in form_data.items():
         python_type = tool.arg_types[key]
 
         if isinstance(value, StarUploadFile):
-            with open(temp_dir / value.filename, "wb+") as f:
+            upload_path = temp_dir / value.filename
+            with open(upload_path, "wb+") as f:
                 f.write(value.file.read())
-            kwargs[key] = temp_dir / value.filename
+            kwargs[key] = upload_path
         elif get_origin(python_type) is Literal:
             kwargs[key] = str(value)
         else:
@@ -239,8 +242,8 @@ async def run_isolated(
     with open(temp_dir / "args.json", "w") as f:
         serializer.dump(kwargs, f)
 
-    # DANGER: user submitted code
-    isolate.run(Path(tool.file), Path(temp_dir))
+    # !DANGER! user submitted code
+    isolate.run(temp_tool, temp_dir)
 
     results_file = temp_dir / "result.json"
     if not results_file.exists():
